3a84eaf9a46650cfee3720eb5f5f3fd7c6af7ff4
peter bull <17122363+peterbull@users.noreply.github.com>
2026-04-26 11:15:14 -0400

    update build for debug

--- theograd/build.zig
+++ theograd/build.zig
@@ -84,6 +84,9 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
+    // debug
+    exe.root_module.strip = false;
+
     // This declares intent for the executable to be installed into the
     // install prefix when running `zig build` (i.e. when executing the default
     // step). By default the install prefix is `zig-out/` but can be overridden
